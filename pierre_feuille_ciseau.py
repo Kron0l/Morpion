b0fe46f7e7087ec7399d29dd7dac94a2115842a0
Tristan <tboda@gaming.tech>
2022-11-09 00:07:49 +0100

    first commit

---
+++ pierre_feuille_ciseau.py
@@ -0,0 +1,40 @@
+import random
+winText="Bravo tu as gagné"
+looseText="Dommage tu as perdu"
+botPlay = "null"
+possiblePlay = ["pierre","feuille","ciseaux"]
+botPlay = random.randint(0,2)
+botPlayName = possiblePlay[botPlay]
+playerPlay="neutre"
+scorePlayer=0
+scoreBot=0
+
+while playerPlay!="stop" :
+    playerPlay="neutre"
+    botPlay = random.randint(0,2)
+    botPlayName = possiblePlay[botPlay]
+    while playerPlay=="neutre":
+        #print (botPlayName)
+        playerPlay = input("Que joues-tu ?")
+    if playerPlay!="stop" :
+        playerPlay = possiblePlay.index(playerPlay)
+        if playerPlay == botPlay :
+            print("Égalité")
+        else :
+            if playerPlay-botPlay==2 or botPlay-playerPlay==2:
+                if playerPlay==0:
+                    print(winText)
+                    scorePlayer=scorePlayer+1
+                else :
+                    print(looseText)
+                    scoreBot=scoreBot+1
+            elif playerPlay>botPlay:
+                print(winText)
+                scorePlayer=scorePlayer+1
+            elif playerPlay<botPlay:
+                print(looseText)
+                scoreBot=scoreBot+1
+    
+    #print (playerPlay)
+    print(scorePlayer,":",scoreBot)
+     
