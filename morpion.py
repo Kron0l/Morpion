c40950c32b255a641cc5f0861cb1414b96dfe43d
Tristan <tboda@gaming.tech>
2022-11-16 15:40:33 +0100

    morpion pseudo avec pseudo code

--- morpion.py
+++ morpion.py
@@ -1,6 +1,6 @@
 #on admet la fonction global qui permet un transfert de variable san utiliser de return
-
 #on admet le fonction event qui permet de récupérer les cliques et mouvement de la souris
+#on admet la fonction quit qui stop le programme
 
 #on considère la fonction tk qui perment de réaliser du graphique
 import tkinter as tk
@@ -36,73 +36,77 @@ def initTableau():
     c=[1,2,3]
     #créer la liste l contenant les terme 1,2,3
     l=[1,2,3]
-    #créer une liste contenant les coordonnées de chaque cellules grâce à la liste l et la liste c
+    #créer une liste nommée tableau contenant les coordonnées de chaque cellules grâce à la liste l et la liste c
     tableau=[[(l[x],c[y]) for y in range (3)]for x in range(3)]
-    #
+    #créer une liste nommée tableAnalyse contenant 9 fois "_"
     tableAnalyse=["_" for i in range(9)]
 
 
 
-#
+#définir la fonction fenêtre qui permet de remplir la fenêtre principale avec la grille de morpion
 def fenetre():
-
-#
+    
+    #pour x de 0 à board_size
     for x in range(board_size):
 
-#
+        #alors
+        #pour y de 0 à board_size
         for y in range(board_size):
 
-#
+            #créer un rectangle ayant comme points opposés : y * cell_size; x * cell_size et y * cell_size + cell_size; x * cell_size + cell_size,
             canvas.create_rectangle(
-                #
+                        
                         y * cell_size,
                         x * cell_size,
-                        #
                         y * cell_size + cell_size,
-                        #
                         x * cell_size + cell_size,
-                        #
+                        
                     )
 
 
-#
+#définir la fonction reinit servant à remettre les valeur à 0
 def reinit():
 
-#
+    #récupérer la variable victoire grâce à la fonction global
     global victoire
     
-    #
+    #assigner la valeur "neutre" à la variable victoire
     victoire="neutre"
-    #
+    #supprimer tous les dessin de la page principale
     canvas.delete(ALL)
-    #
+    #invoquer la fonction fenêtre
     fenetre()   
-    #
+    #invoquer la fonction initTableau
     initTableau()
 
 
-#
+#définir la fonction croix contenant le paramètre cell et permettant de faire le dessin d'une croix
 def croix(cell):
 
-#
+    #créer une des 2 diagonales de la croix par rapport a cell et cell_size
     canvas.create_line(cell[0] * cell_size + 10, cell[1] * cell_size + 10, (cell[0] + 1) * cell_size - 10, (cell[1] + 1) * cell_size - 10, width = 5, fill = "red")
-    #
+    #créer l'autre diagonale de la croix par rapport a cell et cell_size
     canvas.create_line(cell[0] * cell_size + 10, (cell[1] + 1) * cell_size - 10, (cell[0] + 1) * cell_size - 10, cell[1] * cell_size + 10, width = 5, fill = "red")
-    #
+    #insérer insert_croix dans le tableau en position cell[0]][cell[1]
     tableau[cell[0]][cell[1]]=tableau[cell[0]][cell[1]]+insert_croix
-    #
+    #écrire tbleau dans le terminal
     print("tableau",tableau[cell[0]][cell[1]])
 
 
 
+#définir la fonction rond ayant comme paramètre cell et permettant de déssiner un rond
 def rond(cell):
 
+    #créer un oval par rapport à cell et cell_size
     canvas.create_oval (cell[0] *cell_size+10, cell[1] * cell_size + 10, (cell[0] + 1) * cell_size - 10, (cell[1] + 1) * cell_size - 10, width = 5, outline="blue")
+    #insérer insert_rond dans le tableau en position cell[0]][cell[1]
     tableau[cell[0]][cell[1]]=tableau[cell[0]][cell[1]]+insert_rond
+    #écrire tbleau dans le terminal
     print("tableau",tableau[cell[0]][cell[1]])
 
 
 
+#définir la fonction analyse permettant de faire ue analyse de chaque cellule dut tableau et donner la victoire si nécessaire
 def analyse():
 
     global tableau,tableAnalyse,victoire
@@ -202,70 +206,108 @@ def analyse():
 
 
 
-def draw(cell,tableau):
+#définir game avec les paramètre cell et tableau
+def game(cell,tableau):
 
+    #récupérer flag et victoire grâce a la fonction global
     global flag,victoire
 
+    #si flag
     if flag : 
 
+        #alors
+        #si la longueur de tableau en position [cell[0]][cell[1]] est égale à 2
         if len(tableau [cell[0]][cell[1]])==2:
-
+            
+            #invoquer la fonction croix avec le paramètre cell
             croix(cell)
+            #inverser la valeur de flag
             flag=not(flag)   
 
+    #sinon
     else:
-
+        
+        #alors
+        #si la longueur de tableau e position [cell[0]][cell[1]] est égale à 2
         if len(tableau [cell[0]][cell[1]])==2:
 
+            #invoquer la fonction rond avec le paramètre cell
             rond(cell)
+            #inverser la valeur de flag
             flag=not(flag)
 
+    #invoquer la fonction analyse
     analyse()
 
 
+    #si victoire est différent de "neutre"
     if victoire!="neutre":
-
+        
+        #alors
+        #ouvrir une nouvelle fenêtre
         fen_princ = Tk()
 
+        #si victoire est différent de "égalité"
         if victoire!="égalité":
 
+            #alors
+            #afficher "Les gagnant sont les " victoire en police courrier de taille 30 et de couleur '#FF6200' (orange)
             monAffichage = Label(fen_princ, text="Les gagnat sont les "+victoire,font=("Courrier",30), width=0, fg ='#FF6200')
             monAffichage.pack()
-            
+        
+        #sinon
         else:
 
+            #alors
+            #afficher #afficher "Égalité" en police courrier de taille 30 et de couleur '#FF6200' (orange) 
             monAffichage = Label(fen_princ, text="Égalité",padx=50, font=("Courrier",30), width=0, fg ='#FF6200')
             monAffichage.pack()
- 
-        monBouton = Button(fen_princ, text="Réinitialiser", command=lambda:[reinit(),fen_princ.destroy()])
-        monBouton.pack()
-        
-        monBouton2 = Button(fen_princ, text="Quitter", command=quit)
-        monBouton2.pack()
 
+        #créer un boutton réinitialiser avec comme texte "Réinitialiser" et qui invoque la fonction reinit et détruit la fen_princ
+        reinitialiser = Button(fen_princ, text="Réinitialiser", command=lambda:[reinit(),fen_princ.destroy()])
+        reinitialiser.pack()
+        
+        #créer un boutton quitter avec comme texte "Quitter" et qui invoque la fonction quit
+        quitter = Button(fen_princ, text="Quitter", command=quit)
+        quitter.pack()
+
+    #écrire tableau dans le termiinal
     print(tableau)
 
 
-
+#définir la fonction afficher avec comme paramètre event
 def afficher(event) :
 
+    #récupérer victoire grâce à la fonction global
     global victoire
     
+    # si victoire est égal à "neutre"
     if victoire=="neutre":
 
+        #alors
+        #assigner la valeur de event.x à abcisse
         abscisse = event.x
+        #assigner la valeur de event.y à ordonnee
         ordonnee = event.y
+        #assigner à cell la liste [int (abscisse / cell_size), int (ordonnee/cell_size)] (coordonnée de la cellule cliqué)
         cell=[int (abscisse / cell_size), int (ordonnee/cell_size)]
+        #écrire abcisse,ordonne,cell dans le terminal
         print (abscisse,ordonnee,cell)
-        draw(cell,tableau)
+        #invoquer game avec les paramètre cell et tableau
+        game(cell,tableau)
 
-
+#créer une fenêtre de longueur canvas_size et de largeur canvas_size
 canvas = tk.Canvas(root, width=canvas_size, height=canvas_size)
+#invoquer la fonction fenetre
 fenetre()
+#invoquer la fonction initTableau
 initTableau()
 canvas.pack()
 
+#afficher(tableau)
 print (tableau)
+#afficher tableAnalyse
 print (tableAnalyse)
+#invoquer la fonction afficher <Button-1> est cliqué (clique gauche)
 canvas.bind('<Button-1>', afficher)
 root.mainloop()
